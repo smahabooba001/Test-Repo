08f7353cfef0d407b353e7158470d58e6fa8397d
Samreen Havaldar <samreenbanu.mahaboobali.havaldar@pwc.com>
2026-04-16 15:24:58 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int value1, value2;
+
+    printf("Enter first value: ");
+    scanf("%d", &value1);
+
+    printf("Enter second value: ");
+    scanf("%d", &value2);
+
+    printf("You entered: %d and %d\n", value1, value2);
+
+    return 0;
+}
